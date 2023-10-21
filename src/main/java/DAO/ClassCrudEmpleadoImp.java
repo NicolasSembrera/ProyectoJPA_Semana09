9064cfe5d9d1aebe75e656673e0dc587bba8da58
NicolasSembrera <nsembrera@gmail.com>
2023-10-21 17:40:50 -0500

    Listado

---
+++ src/main/java/DAO/ClassCrudEmpleadoImp.java
@@ -0,0 +1,106 @@
+package DAO;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import Interfaces.Inempeadoable;
+import model.TblEmpleado;
+
+public class ClassCrudEmpleadoImp implements Inempeadoable {
+
+	public void RegistrarEmpleado(TblEmpleado tblemp) {
+		        //establecemos la conexion con la unidad de persistencia...
+				EntityManagerFactory conex=Persistence.createEntityManagerFactory("JPASabado");
+				//gestionar entidades como registrar,actualizar,eliminar,etc....
+				 EntityManager emanager=conex.createEntityManager();
+				//iniciamos la transaccion....
+				 emanager.getTransaction().begin();
+				 //insertamos en la base de datos
+				 emanager.persist(tblemp);
+				 //emitimo un mensaje por consola
+				 System.out.println("Dato registrado en BD");
+				 //comfirmamos´
+				 emanager.getTransaction().commit();
+				 //cerramos 
+				 emanager.close();
+		
+	}   //fin del metodo registrar.....
+
+	public void ActualizarEmpleado(TblEmpleado tblemp) {
+		        //establecemos la conexion con la unidad de persistencia...
+				EntityManagerFactory conex=Persistence.createEntityManagerFactory("JPASabado");
+				//gestionar entidades como registrar,actualizar,eliminar,etc....
+				EntityManager emanager=conex.createEntityManager();
+				//iniciamos la transaccion....
+				emanager.getTransaction().begin();
+				//actualizamos los registros...
+				emanager.merge(tblemp);
+				//emitimos un mensaje por consola
+				System.out.println("Dato actulizado en la bd");
+				//confirmamos
+				emanager.getTransaction().commit();
+				//cerramos
+				emanager.close();
+				
+		
+	}    //fin del metodo actualizar....
+
+
+	public void EliminarEmpleado(TblEmpleado tblemp) {
+		//establecemos la conexion con la unidad de persistencia...
+				EntityManagerFactory conex=Persistence.createEntityManagerFactory("JPASabado");
+				//gestionar entidades como registrar,actualizar,eliminar,etc....
+				EntityManager emanager=conex.createEntityManager();
+				//iniciamos la transaccion....
+				emanager.getTransaction().begin();	
+				//recuperamos el codigo a eliminar..
+				TblEmpleado elim=emanager.merge(tblemp);
+				emanager.remove(elim);
+				System.out.println("dato eliminado de la bd");
+				//comfirmamos 
+				emanager.getTransaction().commit();
+				//cerramos
+				emanager.close();
+		
+	}   //fin del metodo eliminar...
+
+
+	public List<TblEmpleado> ListadoEmpleado() {
+		         //establecemos la conexion con la unidad de persistencia...
+				EntityManagerFactory conex=Persistence.createEntityManagerFactory("JPASabado");
+				//gestionar entidades como registrar,actualizar,eliminar,etc....
+				EntityManager emanager=conex.createEntityManager();
+				//iniciamos la transaccion....
+				emanager.getTransaction().begin();
+				
+				List<TblEmpleado> listado=emanager.createQuery("select e from TblEmpleado e",TblEmpleado.class).getResultList();
+		
+				//comfirmamos
+				emanager.getTransaction().commit();
+				
+				//cerramos
+				emanager.close();
+				
+				return listado;
+	}    //fin del metodo listar...
+
+
+	public TblEmpleado BuscarEmpleadoCodigo(TblEmpleado tblemp) {
+		       //establecemos la conexion con la unidad de persistencia...
+				EntityManagerFactory conex=Persistence.createEntityManagerFactory("JPASabado");
+				//gestionar entidades como registrar,actualizar,eliminar,etc....
+				EntityManager emanager=conex.createEntityManager();
+				//iniciamos la transaccion....
+				emanager.getTransaction().begin();			
+				TblEmpleado buscar=emanager.find(TblEmpleado.class,tblemp.getIdempleado());		
+				//confirmamos
+				emanager.getTransaction().commit();
+				//cerramos
+				emanager.close();
+			    return buscar;
+	}    //fin del metodo buscar....
+
+}   //fin de la clase...
